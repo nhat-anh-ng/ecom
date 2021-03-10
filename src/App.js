2886c1a4c4168daff4793400fa8ba5f1b6b02089
Nhat <nhatanh1nguyen@gmail.com>
2021-03-10 23:25:14 +0100

    ecom

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import data from "./data";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="grid-container">
+      <header className="row header">
+        <div>
+          <a className="brand" href="/">
+            Amazon
+          </a>
+        </div>
+        <div>
+          <a href="/signin">Sign In</a>
+          <a href="/cart">Cart</a>
+        </div>
       </header>
+      <main>
+        <div>
+          <div class="row center">
+            {data.products.map((product) => (
+              <div key={product._id} className="card">
+                <a href={`/product/${product._id}`}>
+                  <img className="medium" src={product.image} alt="" />
+                </a>
+                <div className="card-body">
+                  <a href={`/product/${product._id}`}>
+                    <h2>{product.name}</h2>
+                  </a>
+                  <div className="rating">
+                    {data.stars.map((star) => (
+                      <span>
+                        <i className={star.icon}></i>
+                      </span>
+                    ))}
+                  </div>
+                  <div className="price">${product.price}</div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </main>
+      <footer class="row center">All right reserved</footer>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,53 @@
+const data = {
+  products: [
+    {
+      _id: "1",
+      name: "Nike",
+      category: "Shirts",
+      image:
+        "https://cdn.pixabay.com/photo/2021/01/24/20/21/cloud-5946381_960_720.jpg",
+      price: 120,
+      brand: "Nike",
+      description: "cotton",
+    },
+    {
+      _id: "2",
+      name: "Nike",
+      category: "Shirts",
+      image:
+        "https://cdn.pixabay.com/photo/2021/01/24/20/21/cloud-5946381_960_720.jpg",
+      price: 120,
+      brand: "Nike",
+      description: "cotton",
+    },
+    {
+      _id: "3",
+      name: "Nike",
+      category: "Shirts",
+      image:
+        "https://cdn.pixabay.com/photo/2021/01/24/20/21/cloud-5946381_960_720.jpg",
+      price: 120,
+      brand: "Nike",
+      description: "cotton",
+    },
+  ],
+  stars: [
+    {
+      icon: "fa fa-star",
+    },
+    {
+      icon: "fa fa-star",
+    },
+    {
+      icon: "fa fa-star",
+    },
+    {
+      icon: "fa fa-star",
+    },
+    {
+      icon: "fa fa-star",
+    },
+  ],
+};
+
+export default data;
