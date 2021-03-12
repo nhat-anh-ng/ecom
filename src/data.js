3d0325e935e11607be7c2d7cf7734958a943157f
Nhat <nhatanh1nguyen@gmail.com>
2021-03-12 23:30:09 +0100

    shopping cart screen

--- src/data.js
+++ src/data.js
@@ -20,7 +20,7 @@ const data = {
       image:
         "https://cdn.pixabay.com/photo/2021/01/24/20/21/cloud-5946381_960_720.jpg",
       price: 120,
-      countInStock: 136,
+      countInStock: 10,
       rating: 1,
       numReviews: 100,
       brand: "Nike",
@@ -31,12 +31,10 @@ const data = {
     {
       _id: "3",
       name: "Nike",
-      numReviews: 5,
       category: "Shirts",
       image:
         "https://cdn.pixabay.com/photo/2021/01/24/20/21/cloud-5946381_960_720.jpg",
       price: 120,
-      countInStock: 0,
       brand: "Nike",
       description: "cotton",
       note: "1",
